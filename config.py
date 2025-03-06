f7bfee5c0659a3ed7cb2d8d6bf9245483ae97ab3
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-03-06 19:56:49 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# Replace these with your actual API details
+API_ID = "282xx710"  # Replace with your API ID Get From my.telegram.org
+API_HASH = "7fc5b3ddghsdxxxb86481ab5eca3"  # Replace with your API Hash Get From my.telegram.org
+BOT_TOKEN = "78249889xxxxffergipu88wmA9u1tOtq8qddzhyL0"  # Replace with your Bot Token Get From @BotFather 
